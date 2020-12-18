8d1b2017a0813337d1d72af376fd0fbf19d215b6
meridsa <mkvindesland@gmail.com>
2020-12-18 15:22:19 +0100

    ngSwitch now uses enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,18 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   fruits = FRUITS;
+  colours = COLOUR;
+}
+
+enum COLOUR {
+  RED = 'red',
+  YELLOW = 'yellow',
+  GREEN = 'green',
+  BLUE = 'blue'
 }
 
 const FRUITS = [
-  {name: 'apple', colour: 'red'},
-  {name: 'banana', colour: 'yellow'},
+  {name: 'apple', colour: COLOUR.RED},
+  {name: 'banana', colour: COLOUR.YELLOW},
   {name: 'made up', colour: null}
 ]
